ed40c5b953a3257ac13c5fbe2f6ce9923112b378
Vy Nguyen <guenhongzhenqiushan@gmail.com>
2025-05-08 12:18:12 +0700

    update logic show list photo(use dataBinding).

--- app/src/main/java/com/app/testpexel/presentation/photo/BindingAdapters.kt
+++ app/src/main/java/com/app/testpexel/presentation/photo/BindingAdapters.kt
@@ -2,12 +2,15 @@ package com.app.testpexel.presentation.photo
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.app.testpexel.utils.GlideEngine
 import com.bumptech.glide.Glide
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, url: String?) {
-    Glide.with(view.context)
-        .load(url)
-        .centerCrop()
-        .into(view)
+    // Tải ảnh
+    GlideEngine.instance?.loadPhotoUrl(
+        view.context,
+        url.toString(),
+        view
+    )
 }
